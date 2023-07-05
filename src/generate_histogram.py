7ad7850dfa447220444cb0111f33a67fd5ba703c
BBripy207 <yael.rincon@train.ia.center>
2023-07-04 17:50:01 -0700

    first commit

---
+++ src/generate_histogram.py
@@ -0,0 +1,26 @@
+# src/generate_figures.py
+import pandas as pd
+import matplotlib.pyplot as plt
+import helpers.dataloader as dtl
+
+
+def plot_histogram(df, column, output_path):
+    plt.figure(figsize=(10, 6))
+    plt.hist(df[column])
+    plt.title(f"Distribución de {column}")
+    plt.xlabel(column)
+    plt.ylabel("Frecuencia")
+
+    plt.savefig(output_path)
+    plt.close()
+
+
+def main():
+    df = pd.read_csv("./data/processed/RH_procesado.csv")
+
+    # Asegúrate de que los datos estén correctamente procesados antes de intentar trazarlos
+    plot_histogram(df, "Desercion", "./reports/figures/histograma_desercion.png")
+
+
+if __name__ == "__main__":
+    main()
